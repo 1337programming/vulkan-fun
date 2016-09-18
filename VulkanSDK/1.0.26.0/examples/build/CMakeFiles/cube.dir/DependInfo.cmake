9c904474dda3474ac898ba5e64c5ceda1b95395a
Patrick Opie <opiepj@plu.edu>
2016-09-17 18:33:03 -0700

    initial commit

---
+++ VulkanSDK/1.0.26.0/examples/build/CMakeFiles/cube.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/patrick.opie/Documents/github/vulkan-fun/VulkanSDK/1.0.26.0/examples/cube.c" "/Users/patrick.opie/Documents/github/vulkan-fun/VulkanSDK/1.0.26.0/examples/build/CMakeFiles/cube.dir/cube.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "VK_USE_PLATFORM_XCB_KHR"
+  "VK_USE_PLATFORM_XLIB_KHR"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/X11R6/include"
+  "/Users/patrick.opie/Documents/github/vulkan-fun/VulkanSDK/1.0.26.0/x86_64/include"
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
